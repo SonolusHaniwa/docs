02f1268fb2df0d8ce0c145c71e2c60cc98be7361
LittleYang0531 <littleyang0531@outlook.com>
2024-07-03 14:51:34 +0000

    Update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 import { viteBundler } from '@vuepress/bundler-vite'
 import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
-import Prism from 'prismjs'
+import Prism from 'prismjs/prism.js'
 import prismjsPlugin from '@vuepress/plugin-prismjs'
 
 export default defineUserConfig({
